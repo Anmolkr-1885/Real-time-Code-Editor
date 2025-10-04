c4db8d2dcfd339006aa9aced578d1a2ba71728c9
ANMOL KUMAR GUPTA <137256041+Anmolkr-1885@users.noreply.github.com>
2025-10-04 16:44:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Editor from "@monaco-editor/react";
 import { useEffect } from "react";
 
-const socket = io("https://localhost:5000");
+const socket = io("https://real-time-code-editor-00mi.onrender.com");
 
 const App = () => {
   const [joined, setJoined] = useState(false);
